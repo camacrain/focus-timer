e9be3e17321dbd603ac7a28b6cd7e3d8ea1b555d
Cam Crain <camcrain@icloud.com>
2024-01-31 22:24:14 -0400

    remove memo from digit component

--- src/components/Digit.js
+++ src/components/Digit.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import styles from './Digit.module.css';
 
-export const Digit = React.memo((props) => {
+export const Digit = (props) => {
     return (
         <div className={styles.digitContainer}>
             <img className={styles.digit} src={props.digit} />
         </div>
     );
-}, (prevProps, nextProps) => {
-    return prevProps.digit === nextProps.digit;
-});
+};
